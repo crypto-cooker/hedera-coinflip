bce79e46c6fbc1c6ef7cd61390105a4aea2c2c77
mateo-kova <mateo-kova0510@gmail.com>
2023-08-08 12:25:40 +0900

    add sauceinu

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,9 @@
 module.exports = [
     {
+        token: "SAUCEINU",
+        amounts: ["50K", "200K", "700K", "1.5M", "5M"]
+    },
+    {
         token: "SAUCE",
         amounts: [10, 50, 200, 500, 1000]
     },
